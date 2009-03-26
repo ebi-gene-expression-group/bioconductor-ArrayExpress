ff0886901fa67367f5f49607ddb37e235d379380
Audrey Kauffmann <audrey@ebi.ac.uk>
2009-03-26 13:17:20 +0000

    Should work even on file names not starting by E-, need to test after Mohammad changes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ArrayExpress@38222 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/extract.zip.r
+++ R/extract.zip.r
@@ -1,36 +1,11 @@
-extract.zip = function (file, unzip = getOption("unzip"), extract_path = dirname(file)) {
-  if (!is.character(unzip) || length(unzip) != 1) 
-    stop("'unzip' must be a single character string")
-  if (!nzchar(unzip)) 
-    unzip = "internal"
+extract.zip = function (file, extract_path = dirname(file)) {
   topic = basename(file)
   path = dirname(file)
   if (file.exists(file.path(path, topic))) {
-    if (unzip != "internal") {
-      list = paste(unzip, "-l", file)
-      cmd = paste(unzip, "-oq", file, " -d ", extract_path)
-      if (.Platform$OS.type == "windows")
-	{
-          rc = try(system(list, intern=T))
-          if(inherits(rc,"try-error"))
-            stop("Cannot extract the files the downloaded archive. Please install unzip on your machine")   
-          rc = paste("E-",na.omit(sapply(1:length(rc), function(i) strsplit(rc," E-")[[i]][2])),sep="")
-          if(length(grep("zip",rc)) != 0)
-            rc = rc[-grep("zip",rc)]
-          res = system(cmd, invisible = TRUE)
-	} else {
-          rc = system(list, intern=T)##works
-          rc = paste("E-",na.omit(sapply(1:length(rc), function(i) strsplit(rc," E-")[[i]][2])),sep="")
-          if(length(grep("zip",rc)) != 0)
-            rc = rc[-grep("zip",rc)]
-          res = system(paste(cmd, "> /dev/null"))
-        }
-    }
-    else {
-      rc = unzip(zipfile = file.path(path, topic), files = NULL, list = TRUE, exdir = path) ##works
-      rc = as.character(rc[,1])
-    }
-  } else stop(sprintf("%s does not exist",file.path(path, topic)))
-  
+    rc = unzip(zipfile = file.path(path, topic), list = TRUE, exdir = extract <- path)
+    rc = as.character(rc[,1])
+    if(inherits(rc,"try-error"))
+      stop("Cannot extract the files the downloaded archive. Please install unzip on your machine")   
+  } else stop(sprintf("%s does not exist",file.path(path, topic)))  
   return(rc)
 }

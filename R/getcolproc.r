20339b7ddab3c90637c25163f95267bc47d45a81
Audrey Kauffmann <audrey@ebi.ac.uk>
2009-09-04 12:24:35 +0000

    expression values of processed ExpressionSet are numeric
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ArrayExpress@41510 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getcolproc.r
+++ R/getcolproc.r
@@ -3,7 +3,7 @@ getcolproc = function(files)
     path = files$path
     procfile = files$procfile[1]
     coln = scan(file.path(path,procfile),what = "",nlines = 1, skip = 1, sep = "\t")
-    return(coln)
+    return(unique(coln))
   }
 
 getcolraw = function(path, rawfiles)
--- R/procset.r
+++ R/procset.r
@@ -1,28 +1,27 @@
 procset = function(files, procol)
-  {
-    path = files$path
-    procfile = files$procfile
-    sdrf = files$sdrf
-    idf = files$idf
-    adf = files$adf
-
-    proctot = read.table(file.path(path,procfile),header=TRUE,sep="\t")
-    procsel = proctot[-1, procol == proctot[1,]]
+{
+  stopifnot(length(procol)==1)
+  with(files, {
+    
+    proctot = read.table(file.path(path,procfile),header=TRUE,sep="\t", quote="", row.names=1, stringsAsFactors=FALSE)
+    procsel = matrix(as.numeric(as.matrix(proctot[-1, procol == proctot[1,]])), nrow=nrow(proctot)-1)
     colnames(procsel) = colnames(proctot[, procol == proctot[1,]])
-    rownames(procsel) = proctot[-1,1]
-
+    rownames(procsel) = rownames(proctot[-1,])
+   
     proceset = new("ExpressionSet",
-      exprs = as.matrix(procsel))
+      exprs = procsel)
 
     ph = try(read.AnnotatedDataFrame(sdrf, path = path, row.names=NULL, blank.lines.skip = TRUE, fill=TRUE, varMetadata.char="$"))
 
     phenoData(proceset) = ph
 
-    procesetex = try(creating_experiment(idf = idf, eset = proceset, path = path))
+    procesetex = try(creating_experiment(idf = idf, eset = proceset, path = path)) 
     if(!inherits(procesetex, 'try-error'))
       proceset = procesetex else warning("Cannot attach experimentData")
     
     procesetex = try(addADF(adf = adf, eset = proceset, path = path))
     if(!inherits(procesetex, 'try-error'))
-      proceset = procesetex else warning("Cannot attach featureData")   
-  }
+      proceset = procesetex else warning("Cannot attach featureData")
+    return(proceset)
+  }) ## with
+}

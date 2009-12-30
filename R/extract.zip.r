f76d795eba03b77eb3311d15d7e936a7fdc09e18
Audrey Kauffmann <audrey@ebi.ac.uk>
2009-12-30 18:15:55 +0000

    Corrected extraction of multiple zip
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ArrayExpress@43797 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/extract.zip.r
+++ R/extract.zip.r
@@ -1,10 +1,11 @@
-extract.zip = function (file, extract_path = dirname(file)) {
+extract.zip = function (file, extractpath = dirname(file)[1]) {
   topic = basename(file)
   path = dirname(file)
   if (file.exists(file.path(path, topic))) {
-    unzip(zipfile = file.path(path, topic), exdir = extract <- path)
-    rc = unzip(zipfile = file.path(path, topic), list = TRUE, exdir = extract <- path)
-    rc = as.character(rc[,1])
+    lapply(1:length(file), function(i) unzip(zipfile = file.path(path[i], topic[i]), exdir = extractpath))
+    rc = lapply(1:length(file), function(i) unzip(zipfile = file.path(path[i], topic[i]), list = TRUE, exdir = extractpath))
+    if(length(rc) > 1)
+      rc = do.call(c, sapply(1:length(file), function(i) as.character(rc[[i]][,1]))) else rc = as.character(rc[[1]][,1])    
     if(inherits(rc,"try-error"))
       stop("Cannot extract the files the downloaded archive. Please install unzip on your machine")   
   } else stop(sprintf("%s does not exist",file.path(path, topic)))  
